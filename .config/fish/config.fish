86b60a7b1e204df9d23b5853947d5f86f94e9fc3
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-07-13 09:40:57 +0800

    Track fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,195 @@
+abbr -a yr 'cal -y'
+abbr -a c cargo
+abbr -a e nvim
+abbr -a m make
+abbr -a o open
+abbr -a g git
+abbr -a gc 'git checkout'
+abbr -a ga 'git add -p'
+abbr -a gst 'git status'
+abbr -a vimdiff 'nvim -d'
+abbr -a ct 'cargo t'
+abbr -a cls "clear && printf '\e[3J'"
+
+set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/bin /bin
+
+if status --is-interactive
+	tmux ^ /dev/null; and exec true
+end
+
+if command -v exa > /dev/null
+	abbr -a l 'exa'
+	abbr -a ls 'exa'
+	abbr -a ll 'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a lll 'ls -la'
+end
+
+if command -v rmtrash > /dev/null
+	abbr -a del 'rmtrash'
+end
+
+if test -f /usr/share/autojump/autojump.fish;
+	source /usr/share/autojump/autojump.fish;
+end
+
+function ssh
+	switch $argv[1]
+	case "*.amazonaws.com"
+		env TERM=xterm /usr/bin/ssh $argv
+	case "ubuntu@"
+		env TERM=xterm /usr/bin/ssh $argv
+	case "*"
+		/usr/bin/ssh $argv
+	end
+end
+
+function apass
+	if test (count $argv) -ne 1
+		pass $argv
+		return
+	end
+
+	asend (pass $argv[1] | head -n1)
+end
+
+function qrpass
+	if test (count $argv) -ne 1
+		pass $argv
+		return
+	end
+
+	qrsend (pass $argv[1] | head -n1)
+end
+
+function asend
+	if test (count $argv) -ne 1
+		echo "No argument given"
+		return
+	end
+
+	adb shell input text (echo $argv[1] | sed -e 's/ /%s/g' -e 's/\([[()<>{}$|;&*\\~"\'`]\)/\\\\\1/g')
+end
+
+function qrsend
+	if test (count $argv) -ne 1
+		echo "No argument given"
+		return
+	end
+
+	qrencode -t ASCII $argv[1]
+end
+
+function limit
+	numactl -C 0,1,2 $argv
+end
+
+function remote_alacritty
+	# https://gist.github.com/costis/5135502
+	set fn (mktemp)
+	infocmp alacritty > $fn
+	scp $fn $argv[1]":alacritty.ti"
+	ssh $argv[1] tic "alacritty.ti"
+	ssh $argv[1] rm "alacritty.ti"
+end
+
+# Type - to move up to top parent dir which is a repository
+function d
+	while test $PWD != "/"
+		if test -d .git
+			break
+		end
+		cd ..
+	end
+end
+
+# Fish git prompt
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream 'none'
+set -g fish_prompt_pwd_dir_length 3
+
+# colored man output
+# from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+setenv LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+setenv LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+setenv LESS_TERMCAP_me \e'[0m'           # end mode
+setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
+setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+setenv LESS_TERMCAP_ue \e'[0m'           # end underline
+setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
+# Build cache
+setenv RUSTC_WRAPPER 'sccache'
+
+# For RLS
+# https://github.com/fish-shell/fish-shell/issues/2456
+setenv LD_LIBRARY_PATH (rustc +nightly --print sysroot)"/lib:$LD_LIBRARY_PATH"
+setenv RUST_SRC_PATH (rustc --print sysroot)"/lib/rustlib/src/rust/src"
+
+setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
+setenv FZF_CTRL_T_COMMAND 'fd --type file --follow'
+setenv FZF_DEFAULT_OPTS '--height 20%'
+
+# Fish should not add things to clipboard when killing
+# See https://github.com/fish-shell/fish-shell/issues/772
+set FISH_CLIPBOARD_CMD "cat"
+
+function fish_user_key_bindings
+	bind \cz 'fg>/dev/null ^/dev/null'
+	if functions -q fzf_key_bindings
+		fzf_key_bindings
+	end
+end
+
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color blue
+	echo -n (hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '| '
+	set_color normal
+end
+
+#function fish_greeting
+#	echo
+#	echo -e (uname -rs | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+#	echo -e (uptime | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+#	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+#	echo
+#
+#	set_color normal
+#end
+
+function fish_greeting
+    echo '                 '(set_color F00)'___
+  ___======____='(set_color FF7F00)'-'(set_color FF0)'-'(set_color FF7F00)'-='(set_color F00)')
+/T            \_'(set_color FF0)'--='(set_color FF7F00)'=='(set_color F00)')    '(set_color red)(whoami)'@'(hostname)'
+[ \ '(set_color FF7F00)'('(set_color FF0)'0'(set_color FF7F00)')   '(set_color F00)'\~    \_'(set_color FF0)'-='(set_color FF7F00)'='(set_color F00)')'(set_color yellow)'    Uptime:'(set_color white)(uptime | sed 's/.*up \([^,]*\), .*/\1/')(set_color red)'
+ \      / )J'(set_color FF7F00)'~~    \\'(set_color FF0)'-='(set_color F00)')    IP Address: '(set_color white)(ipconfig getifaddr en0)(set_color red)'
+  \\\\___/  )JJ'(set_color FF7F00)'~'(set_color FF0)'~~   '(set_color F00)'\)     '(set_color yellow)'Version: '(set_color white)(echo $FISH_VERSION)(set_color red)'
+   \_____/JJJ'(set_color FF7F00)'~~'(set_color FF0)'~~    '(set_color F00)'\\
+   '(set_color FF7F00)'/ '(set_color FF0)'\  '(set_color FF0)', \\'(set_color F00)'J'(set_color FF7F00)'~~~'(set_color FF0)'~~     '(set_color FF7F00)'\\
+  (-'(set_color FF0)'\)'(set_color F00)'\='(set_color FF7F00)'|'(set_color FF0)'\\\\\\'(set_color FF7F00)'~~'(set_color FF0)'~~       '(set_color FF7F00)'L_'(set_color FF0)'_
+  '(set_color FF7F00)'('(set_color F00)'\\'(set_color FF7F00)'\\)  ('(set_color FF0)'\\'(set_color FF7F00)'\\\)'(set_color F00)'_           '(set_color FF0)'\=='(set_color FF7F00)'__
+   '(set_color F00)'\V    '(set_color FF7F00)'\\\\'(set_color F00)'\) =='(set_color FF7F00)'=_____   '(set_color FF0)'\\\\\\\\'(set_color FF7F00)'\\\\
+          '(set_color F00)'\V)     \_) '(set_color FF7F00)'\\\\'(set_color FF0)'\\\\JJ\\'(set_color FF7F00)'J\)
+                      '(set_color F00)'/'(set_color FF7F00)'J'(set_color FF0)'\\'(set_color FF7F00)'J'(set_color F00)'T\\'(set_color FF7F00)'JJJ'(set_color F00)'J)
+                      (J'(set_color FF7F00)'JJ'(set_color F00)'| \UUU)
+                       (UU)'(set_color normal)
+end
+
+zoxide init fish | source
---
+++ .config/fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/Users/jieyouxu/.fzf/shell/key-bindings.fish
