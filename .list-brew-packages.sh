5e2cfc03967ac481e3c2303a3f6e530ac5b4b4f0
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-08-28 10:20:47 +0800

    infra: fix list-brew-packages script

--- .list-brew-packages.sh
+++ .list-brew-packages.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-DEFAULT_LIST_FILENAME="~/.brewlist"
-CASK_LIST_FILENAME="~/.brewcasklist"
+DEFAULT_LIST_FILENAME="$HOME/.brewlist"
+CASK_LIST_FILENAME="$HOME/.brewcasklist"
 
-brew list > DEFAULT_LIST_FILENAME && \
-	brew cask list > CASK_LIST_FILENAME
+if ! command -v brew &> /dev/null
+then
+	cat <<- END
+	ERROR: program \`brew\` could not be found, please make sure you've
+	installed it and \`brew`\ exists in your \`$PATH\`
+	END
+	exit
+fi
+
+brew list > $DEFAULT_LIST_FILENAME
+brew cask list > $CASK_LIST_FILENAME
 
 echo -e "\x1B[36mbrew list: \x1B[0m";
-cat DEFAULT_LIST_FILENAME;
+cat $DEFAULT_LIST_FILENAME;
 echo "";
 
 echo -e "\x1B[36mbrew cask list: \x1B[0m";
-cat CASK_LIST_FILENAME;
+cat $CASK_LIST_FILENAME;
